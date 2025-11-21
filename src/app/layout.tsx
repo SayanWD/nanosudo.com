e047f82e09178d8b1c4be6d06ade51a49f6296c9
Sayan <roorsayan@gmail.com>
2025-11-21 20:25:02 +0500

    Add GTM integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
@@ -12,7 +13,45 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { readonly children: ReactNode }): ReactNode {
   return (
     <html suppressHydrationWarning data-theme="dark">
-      <body>{children}</body>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NC3L984P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-base" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NC3L984P');
+          `}
+        </Script>
+
+        {/* Google tag (gtag.js) - GA4 */}
+        <Script
+          id="ga4-base"
+          src="https://www.googletagmanager.com/gtag/js?id=G-L02KBFS5DK"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-config" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-L02KBFS5DK');
+          `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   );
 }
